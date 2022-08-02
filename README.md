a735d0ed76495112bfb8acf0d50cd87fa329ab1b
marciocamello <marciocamello@oulook.com>
2022-08-02 11:22:38 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# edge-cloudfware-work-test
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Router } from 'worktop';
+import { listen } from 'worktop/cache';
+import { Redis } from '@upstash/redis/cloudflare';
+
+const routes = new Router();
+
+declare global {
+    var UPSTASH_REDIS_REST_URL: string;
+    var UPSTASH_REDIS_REST_TOKEN: string;
+}
+
+const redis = new Redis({
+    url: UPSTASH_REDIS_REST_URL,
+    token: UPSTASH_REDIS_REST_TOKEN
+});
+
+routes.add('GET', '/get', async (req, res) => {
+    const access_count = await redis.get('access_count');
+
+    return res.send(200, { access_count });
+});
+
+routes.add('POST', '/incr', async (req, res) => {
+    const access_count = await redis.incr('access_count');
+
+    return res.send(200, { access_count });
+
+});
+
+listen(routes.run);
